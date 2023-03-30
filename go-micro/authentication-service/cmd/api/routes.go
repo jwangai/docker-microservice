bc129e5e3d08bcedeaadb25b6e55c592cf1852b2
Jackson <juxtarr@gmail.com>
2023-03-30 11:58:02 +0300

    Debug update

--- go-micro/authentication-service/cmd/api/routes.go
+++ go-micro/authentication-service/cmd/api/routes.go
@@ -21,5 +21,8 @@ func (app *Config) routes() http.Handler {
 		MaxAge:           300,
 	}))
 
+	mux.Use(middleware.Heartbeat("/ping"))
+
+	mux.Post("/authenticate", app.Authenticate)
 	return mux
 }
